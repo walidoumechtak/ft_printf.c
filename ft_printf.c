cb3c323f93ae4391ebc1ea9265ebfe31e33da143
Walid Oumechtak <woumecht@e1r2p9.1337.ma>
2022-11-14 21:51:28 +0100

    walid

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,13 @@
 /*   By: woumecht <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 15:29:10 by woumecht          #+#    #+#             */
-/*   Updated: 2022/11/05 15:20:31 by woumecht         ###   ########.fr       */
+/*   Updated: 2022/11/07 15:04:30 by woumecht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+
 
 int	ft_printf(const char *s, ...)
 {
@@ -34,4 +36,11 @@ int	ft_printf(const char *s, ...)
 	}
 	va_end(ptr);
 	return (cpp);
+}
+
+int main()
+{
+	printf("---%k---");
+
+	// getline();
 }
